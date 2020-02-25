bf98d2665d1784eb517cb5a73fab8d430e96e964
igor-shafran <igorshafran.personal@gmail.com>
2020-02-25 13:11:47 -0500

    added shopping cart dropdown, added logic to handle mulitple cart items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,16 +1,22 @@
 import { CartActionTypes } from "./cart.types";
+import { AddItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const CartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
-      console.log(action.CartActionTypes);
       return {
         ...state,
         hidden: !state.hidden
+      };
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: AddItemToCart(state.cartItems, action.payload)
       };
     default:
       return state;
