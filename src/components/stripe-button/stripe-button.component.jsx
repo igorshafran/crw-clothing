5ed86aaaaaada3bcc466b96c6be809ab0d955ca7
igor-shafran <igorshafran.personal@gmail.com>
2020-03-08 18:02:08 -0400

    stripe payment integration and update title and fivico

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_pA85zjiXb9q0jkMMeei6f05S008jReKilb";
+
+  const onToken = token => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://sendeyo.com/up/d/f3eb2117da"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      onToken={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+export default StripeCheckoutButton;
